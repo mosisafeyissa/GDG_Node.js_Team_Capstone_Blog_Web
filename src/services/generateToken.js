3af34d8580dd793a8e7947bc670f4061a930205b
kaalebdarrajjee <kalebdarajje@gmail.com>
2025-04-07 14:27:24 +0300

    post CRUDs done

--- src/services/generateToken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-// Generate a JWT token for user authentication
-const generateToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
-};
-
-module.exports = generateToken;
--- src/services/tokenService.js
+++
@@ -1,7 +0,0 @@
-//simulation
-const sendResetEmail = (email, resetToken) => {
-  console.log(`Password reset link: http://frontend.com/reset-password?token=${resetToken}`);
-  console.log(`Sent to: ${email}`);
-};
-
-module.exports = sendResetEmail;
